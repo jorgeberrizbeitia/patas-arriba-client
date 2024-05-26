a3751a50213bea9319953db822217059f56bbb4c
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2024-05-26 18:33:39 +0200

    fix: fixed small typo

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -161,7 +161,7 @@ function Login() {
 
       <br />
 
-      <Link style={{marginBottom: "20px"}} to="/password-forget">Olvidaste tu contraseña?</Link>
+      <Link style={{marginBottom: "20px"}} to="/password-forget">¿Olvidaste tu contraseña?</Link>
       <Link style={{marginBottom: "20px"}} to="/signup">Si no tienes cuenta, registrate aqui</Link>
     </>
   );
